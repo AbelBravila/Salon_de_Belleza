a971ed002434ceb3fb31f0c710cae804a45df874
AbelBravila <abelbravila0@gmail.com>
2024-05-15 10:22:15 -0600

    landing 30%

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,25 +1,62 @@
 <!DOCTYPE html>
 <html lang="es">
-<head>
+  <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Vividis</title>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
-</head>
-<body>
-<div class="container">
-    <header class="d-flex justify-content-center py-3">
+    <link rel="stylesheet" href="style-landing.css">
+  </head>
+  <body>
+    <nav id="navbar-example2" class="navbar sticky-top navbar-light px-3">
+      <a class="navbar-brand"  href="#inicio"></a>
+      
       <ul class="nav nav-pills">
-        <li class="nav-item"><a href="#" class="nav-link active" aria-current="page">Home</a></li>
-        <li class="nav-item"><a href="#" class="nav-link">INICIO</a></li>
-        <li class="nav-item"><a href="#" class="nav-link">AGENDA TU CITA</a></li>
-        <li class="nav-item"><a href="#" class="nav-link">FAQs</a></li>
-        <li class="nav-item"><a href="#" class="nav-link">NOSOTROS</a></li>
+        <li class="nav-item">
+          <a class="nav-link" href="#inicio" style="color:black;">INICIO</a>
+        </li>
+
+        <li class="nav-item">
+          <a class="nav-link" href="#cita" style="color:black;">AGENDA TU CITA</a>
+        </li>
+        
+        <li class="nav-item">
+          <a class="nav-link" href="#faqs" style="color:black;">FAQs</a>
+        </li>
+
+        <li class="nav-item">
+          <a class="nav-link" href="#nosotros" style="color:black;">NOSOTROS</a>
+        </li>
       </ul>
-    </header>
-  </div>
+    </nav>
+    <main data-bs-spy="scroll" data-bs-target="#navbar-example2" data-bs-offset="0" class="scrollspy-example" tabindex="0">
+      <!--inicio Section Start-->
+      <section id="inicio" class="section">
+        <div id="banner">
+      <img src="img\banner.jpg" alt="banner" style="width:100%;">
+    </div>
+      </section>
+      <!--inicio Section End-->
 
+      <!--Agenda Tu Cita Section Start-->
+      <section id="cita" class="section">
+        <h1>AGENDA TU CITA</h1>
+      </section>
+      <!--Agenda Tu Cita Section End-->
 
-<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous"></script>
-</body>
-</html>
+      <!--Faqs Section Start-->
+      <section id="faqs" class="section">
+        <h1>FAQs</h1>
+      </section>
+      <!--Faqs Section End-->
+
+      <!--Nosotros Section Start-->
+      <section id="nosotros" class="section">
+        <h1>NOSOTROS</h1>
+
+      </section>
+      <!--Nosotros Section End-->      
+    </main>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous"></script>
+  </body>
+  </html>
